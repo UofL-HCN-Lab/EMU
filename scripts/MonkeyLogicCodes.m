9c99ac030d712670feb8c6e89c12ea573be5a92b
Cameron Mattingly <71233634+Ckmatt01@users.noreply.github.com>
2021-03-31 17:53:43 -0400

    moved monkeyLogic codes

---
+++ scripts/MonkeyLogicCodes.m
@@ -0,0 +1,34 @@
+%% MonkeyLogic codes
+% % % % Behavioral Events (eventmarker)
+% % % Start_Trial             = 1;
+% % % Fix_On                  = 2;
+% % % Fix_Off                 = 3;
+% % % Cue_On                  = 4;
+% % % Cue_Off                 = 5;
+% % % Left_Button_On          = 6;
+% % % Right_Button_On         = 7;
+% % %
+% % % Instructions            = 8;
+% % %
+% % % % ML trial start        = 9;
+% % %
+% % % Cong_Left_Left          = 10;
+% % % Cong_Left_Right         = 11;
+% % % Cong_Left_NoResp        = 12;
+% % % Cong_Right_Left         = 13;
+% % % Cong_Right_Right        = 14;
+% % % Cong_Right_NoResp       = 15;S
+% % %
+% % % Abort_Early_Left       = 16;
+% % % Abort_Early_Right      = 17;
+% % %
+% % % % ML trial end          = 18;
+% % %
+% % % Incong_Left_Left        = 19;
+% % % Incong_Left_Right       = 20;
+% % % Incong_Left_NoResp      = 21;
+% % % Incong_Right_Left       = 22;
+% % % Incong_Right_Right      = 23;
+% % % Incong_Right_NoResp     = 24;
+%%%% ML trial start = 9 or 31
+
