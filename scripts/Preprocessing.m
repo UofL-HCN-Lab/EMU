d8def5b17f5f5020f009f10daa13d862c75c2d12
geekygiraffe <geekygiraffe@users.noreply.github.com>
2021-04-02 13:02:02 -0400

    binary trigger

--- scripts/Preprocessing.m
+++ scripts/Preprocessing.m
@@ -1,4 +1,4 @@
-% clear; clc
+clear; clc
 
 %% set path add dependencies
 cd 'C:\Users\jm68130\OneDrive - University of Louisville\library\codeLibrary\github\'
@@ -11,47 +11,79 @@ addpath(genpath('C:\Users\jm68130\OneDrive - University of Louisville\library\co
 addpath('C:\Users\jm68130\OneDrive - University of Louisville\library\codeLibrary\github\fieldtrip');
 ft_defaults
 %% Loads in edf file
-root = 'C:\Users\jm68130\Box\Dept-Neurosurgery_NRGlab\Data\emu';
-subj = 'testTmp';
+root = 'C:\Users\jm68130\Box\Dept-Neurosurgery_NRGlab';
+folder = 'Data\emu'
+subject = 'testTmp';
 file = 'test_ monkeylo_2c734bac-e3b4-4c96-8c1f-a4fb94ca2357.edf';
 ext = '.edf';
-fpath = fullfile(root, subj,file)
+fpath = fullfile(root, folder, subject, file);
+
+cfg = [];
 cfg.dataset = (fpath)
 eegData = ft_preprocessing(cfg);
-%% Creates necessary variables for creating trials
-eegData = eegData.trial{1};
+
+%% find trigger
 eegTrigger = eegData(145,:);
 uniqueValues = unique(eegTrigger);
-eegIdx = find(eegTrigger == uniqueValues(3));
+i = [];k = [];
+for k = [1 2 3 5];
+    for i = 1:size(eegTrigger,2)
+        if eegTrigger(:,i) == uniqueValues(k)
+            eegTrigger(:,i) = 0;
+            
+             elseif eegTrigger(:,i) == uniqueValues(4)
+                 eegTrigger(:,i) = 1;
+        end
+    end
+end
+
+%overwrite tigger channel with binary trigger
+    % 1 trial ongoing
+    % 0 intertrial period
+eegData(145,:) = eegTrigger;
+
+% trial definition based on trigger
+% cfg.data = eegData;
+% cfg.channel = 'all';
+% cfg.trialfun = 'ft_trialfun_general'; % this is the default
+% cfg.trialdef.eventvalue = '1';
+% cfg.trialdef.ntrials = 30;
+% cfg.trialdef.eventtype = 'Trig'
+% cfg.trialdef.prestim = 0; % in seconds
+% cfg.trialdef.poststim = 0; % in seconds
+% ieeg = ft_definetrial(cfg);
+
+%% Finds values of electrode 145 indicating a trial, records them and formats them for the cfg
 count = 0;
 eegStart = [];
 eegEnd = [];
-%% Finds values of electrode 145 indicating a trial, records them and formats them for the cfg
-for a = 1:length(eegIdx)
-    if a==1 %if its the first value (for which there is no reference point)
-        eegStartCurrent = eegIdx(a);
+eegIdx = eegData(145,:);
+
+for i = 1:length(eegIdx)
+    if i==1 %if its the first value (for which there is no reference point)
+        eegStartCurrent = eegIdx(i);
         eegStart = [eegStart;eegStartCurrent];
         continue
     end
-    if a==length(eegIdx) %if its the last value (for which there is no reference point)
+    if i==length(eegIdx) %if its the last value (for which there is no reference point)
         if length(eegEnd) == length(eegStart) %if the trials end with a start
-            eegStartCurrent = eegIdx(a);
+            eegStartCurrent = eegIdx(i);
             eegStart = [eegStart;eegStartCurrent];
             break
         elseif length(eegStart) > length(eegEnd) %if the trials end with a end
-            eegEndCurrent = eegIdx(a);
+            eegEndCurrent = eegIdx(i);
             eegEnd = [eegEnd;eegEndCurrent];
             break
         end
     end
-    if eegIdx(a)~= eegIdx(a+1)-1 %if next value isn't +1 to current value
+    if eegIdx(i)~= eegIdx(i+1)-1 %if next value isn't +1 to current value
         if count == 1; % if its a starting value
             count = 0;
-            eegStartCurrent = eegIdx(a);
+            eegStartCurrent = eegIdx(i);
             eegStart = [eegStart;eegStartCurrent];
         elseif count == 0 % if its a ending value
             count = 1;
-            eegEndCurrent = eegIdx(a);
+            eegEndCurrent = eegIdx(i);
             eegEnd = [eegEnd;eegEndCurrent];
         end
     end
