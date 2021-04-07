d020642e3423f61d6629623c3411da2cddd507f2
Cameron Mattingly <71233634+Ckmatt01@users.noreply.github.com>
2021-04-07 13:41:31 -0400

    Update Preprocessing.m
    Changes Preprocessing's eegTrigger to a binary system. finds incredibly small trials and eliminates them

--- scripts/Preprocessing.m
+++ scripts/Preprocessing.m
@@ -8,11 +8,16 @@ eegData = ft_preprocessing(cfg);
 eegData = eegData.trial{1};
 eegTrigger = eegData(145,:);
 uniqueValues = unique(eegTrigger);
-eegIdx = find(eegTrigger == uniqueValues(3));
+%% Establishes eegTrigger as a binary system. 1 = ongoing trial. 0 = inbetween trials.
+eegTrigger(eegTrigger~=uniqueValues(4))=0;
+eegTrigger(eegTrigger==uniqueValues(4))=1;
+%% establishes necessary variables
+eegIdx = find(eegTrigger == 1);
 count = 0;
 eegStart = [];
 eegEnd = [];
 %% Finds values of electrode 145 indicating a trial, records them and formats them for the cfg
+plot([1:length(eegTrigger)],eegTrigger(:))
 for a = 1:length(eegIdx)
     if a==1 %if its the first value (for which there is no reference point)
         eegStartCurrent = eegIdx(a);
@@ -31,17 +36,18 @@ for a = 1:length(eegIdx)
         end
     end
     if eegIdx(a)~= eegIdx(a+1)-1 %if next value isn't +1 to current value
-        if count == 1; % if its a starting value
-            count = 0;
-            eegStartCurrent = eegIdx(a);
+            eegStartCurrent = eegIdx(a+1);
             eegStart = [eegStart;eegStartCurrent];
-        elseif count == 0 % if its a ending value
-            count = 1;
             eegEndCurrent = eegIdx(a);
             eegEnd = [eegEnd;eegEndCurrent];
         end
-    end
 end
+%% Finds and eliminates flawed trials
+eegDifferences = eegEnd-eegStart;
+eegDifferences = find(eegDifferences > 500);
+eegStart = eegStart(eegDifferences(:));
+eegEnd = eegEnd(eegDifferences(:));
+%% Generates offset and the nx3 matrix used for the trials
 eegZeros = zeros(length(eegEnd),1);       
 eegTrl = cat(2,eegStart,eegEnd,eegZeros);
 %% Changes trl to new trial matrix and recreates data considering new trials
