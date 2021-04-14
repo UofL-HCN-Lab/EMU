99aba1515f191094067a9875fc89e422b1176443
Cameron Mattingly <71233634+Ckmatt01@users.noreply.github.com>
2021-04-14 16:24:57 -0400

    Update Preprocessing.m
    Loads in monkey logic data now and attempts to figure out what is wrong with the start times

--- scripts/Preprocessing.m
+++ scripts/Preprocessing.m
@@ -2,9 +2,13 @@ clear
 clc
 
 %% Loads in edf file
-cfg.dataset = ('test_ monkeylo_2c734bac-e3b4-4c96-8c1f-a4fb94ca2357.edf')
+cfg.dataset = ('NRG~ test_dcb22a20-320f-4d7a-ab4c-ee61522e4b49.eeg')
 eegData = ft_preprocessing(cfg);
+%% Loads in MonkeyLogic data
+[data,MLConfig,TrialRecord,filename,varlist] = mlread('20201211_1121_0000000_test2_SimonTask_v13_ML.bhv2');
+
 %% Creates necessary variables for creating trials
+plot(eegData.trial{1, 1}(1,:))
 eegData = eegData.trial{1};
 eegTrigger = eegData(145,:);
 uniqueValues = unique(eegTrigger);
@@ -44,9 +48,10 @@ for a = 1:length(eegIdx)
 end
 %% Finds and eliminates flawed trials
 eegDifferences = eegEnd-eegStart;
-eegDifferences = find(eegDifferences > 500);
-eegStart = eegStart(eegDifferences(:));
-eegEnd = eegEnd(eegDifferences(:));
+eegDifferencesIdx = find(eegDifferences > 100);
+eegStart = eegStart(eegDifferencesIdx(:));
+eegEnd = eegEnd(eegDifferencesIdx(:));
+eegDifferences = eegDifferences(eegDifferencesIdx(:));
 %% Generates offset and the nx3 matrix used for the trials
 eegZeros = zeros(length(eegEnd),1);       
 eegTrl = cat(2,eegStart,eegEnd,eegZeros);
@@ -56,3 +61,23 @@ cfg.channel = 'all';
 cfg = ft_definetrial(cfg)
 eegData = ft_preprocessing(cfg)
 
+%%
+eegStartDifferences = diff(eegStart)*2.048;
+monkeyLogicStartTimes = diff([data.AbsoluteTrialStartTime])';
+figure(1)
+plot(eegStartDifferences(2:end))
+hold on
+plot(monkeyLogicStartTimes(2:end))
+%%
+
+% %% ASR
+% %Clean data using Automatic Subspace Reconstruction (ASR)
+% % EEG = clean_rawdata(EEG, arg_flatline, arg_highpass, arg_channel, arg_noisy, arg_burst, arg_window)
+% EEG = clean_artifacts(eegData);
+% % - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+% 
+% EEG = clean_rawdata(eegData, 5, [-1], 0.8, 4, 5, -1);
+% vis_artifacts(clean,raw);
+% %%
+% EEG = pop_interp(eegData, cpyEEG.chanlocs, 'spherical');
+% pop_eegplot(cleanedEEG, 1, 1, 1);
